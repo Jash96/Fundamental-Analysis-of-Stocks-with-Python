69ea1dde1164c65e93febe3b36f5c9ab4406d8a4
Jashan Thadani <60536734+Jash96@users.noreply.github.com>
2022-06-15 16:38:14 +0800

    Step 3: execute this to obtain localhost viewer

--- Streamlit visualizer/Sector Analysis Viewer/Fundalit.py
+++ Streamlit visualizer/Sector Analysis Viewer/Fundalit.py
@@ -5,7 +5,7 @@ Created on Tue May 31 16:28:06 2022
 @author: Jashan
 """
 
-# RUN this file on terminal as "streamlit run Fundalit.py" or "!streamlit run Fundalit.py" on IDE
+# RUN this file on terminal as "streamlit run Fundalit.py" or "!streamlit run Fundalit.py" on IDE 
 
 import streamlit as st
 import pandas as pd
